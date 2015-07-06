4539101b6d82dec1005d43e4fa0c98b272a4d666
DANIELYEBER <daniel_leon@upeu.edu.pe>
2015-07-06 18:18:52 -0500

    agregando a las clases Data y IData

---
+++ WebSgeUPeU/src/java/pe/edu/upeu/dao/DepartamentoFiDaoImpl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upeu.dao;
+
+import org.springframework.stereotype.Repository;
+import pe.edu.upeu.modelo.GloDepartamentosFilial;
+import syscenterlife.dmp.core.data.DataAccess;
+
+/**
+ *
+ * @author USERS
+ */
+@Repository("DepartamentoFiDao")
+public class DepartamentoFiDaoImpl extends DataAccess<GloDepartamentosFilial> implements DepartamentoFiDaoInterface{
+    
+}
---
+++ WebSgeUPeU/src/java/pe/edu/upeu/dao/DepartamentoFiDaoInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upeu.dao;
+
+import pe.edu.upeu.modelo.GloDepartamentosFilial;
+import syscenterlife.dmp.core.data.IDataAccess;
+
+/**
+ *
+ * @author USERS
+ */
+public interface DepartamentoFiDaoInterface extends IDataAccess<GloDepartamentosFilial>{
+    
+}
